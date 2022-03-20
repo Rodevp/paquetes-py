06589a6cc76ddb5db8a86f9b6d72310c7dc0e321
Rodevp <rodriguezrodrigor12@gmail.com>
2022-03-20 13:45:14 -0500

    creando modulo ejemplo

---
+++ app.py
@@ -0,0 +1,6 @@
+from modulo import add, div, multiply, res
+
+print(add(2, 3))
+print(div(4,2))
+print(multiply(2,2))
+print(res(5, 3))
---
+++ modulo.py
@@ -0,0 +1,4 @@
+add = lambda x ,y : x + y
+res = lambda x ,y : x - y
+multiply = lambda x ,y : x * y
+div = lambda x ,y : x / y
