b15ffc1acd64aba14c25b99af0cc7a604b8d2ae1
Rodevp <rodriguezrodrigor12@gmail.com>
2022-03-20 14:12:17 -0500

    creando paquete ejemplo

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
+from mi_paquete.fn_mates import Mates
 from modulo import add, div, multiply, res
+
 
 print(add(2, 3))
 print(div(4,2))
 print(multiply(2,2))
 print(res(5, 3))
+
+math = Mates()
+print(math.get_pi())
---
+++ mi_paquete/fn_mates.py
@@ -0,0 +1,9 @@
+class Mates :
+
+    def __init__(self):
+        self.PI = 3.14
+
+    def get_pi(self) :
+        return self.PI
+
+
